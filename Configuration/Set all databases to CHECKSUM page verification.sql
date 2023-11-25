223de9beda07eb706abdeec6a78fab24a7a38585
KingDomain24 <148955967+KingDomain24@users.noreply.github.com>
2023-11-25 13:50:40 -0500

    Create Set all databases to CHECKSUM page verification.sql

---
+++ Configuration/Set all databases to CHECKSUM page verification.sql
@@ -0,0 +1,6 @@
+﻿-- Set all databases to CHECKSUM page verification
+-- Part of the SQL Server DBA Toolbox at https://github.com/DavidSchanzer/Sql-Server-DBA-Toolbox
+-- This script sets the PAGE_VERIFY property for all user databases to CHECKSUM, which is the recommended value
+
+EXEC dbo.sp_foreachdb @command = 'ALTER DATABASE ? SET PAGE_VERIFY CHECKSUM WITH NO_WAIT',
+                      @user_only = 1;
