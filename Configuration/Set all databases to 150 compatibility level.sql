8537224289c0fa1edbf1864104534891bf4e7f3d
KingDomain24 <148955967+KingDomain24@users.noreply.github.com>
2023-11-25 13:49:59 -0500

    Create Set all databases to 150 compatibility level.sql

---
+++ Configuration/Set all databases to 150 compatibility level.sql
@@ -0,0 +1,6 @@
+﻿-- Set all databases to 150 compatibility level
+-- Part of the SQL Server DBA Toolbox at https://github.com/DavidSchanzer/Sql-Server-DBA-Toolbox
+-- This script sets all user databases to Compatibility Level 150 (SQL Server 2019)
+
+EXEC dbo.sp_foreachdb @command = 'ALTER DATABASE ? SET COMPATIBILITY_LEVEL = 150',
+                      @user_only = 1;
