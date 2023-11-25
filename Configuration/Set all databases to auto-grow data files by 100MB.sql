fd4ee9e15cafb76ad555c6c8cc928f12304a453e
KingDomain24 <148955967+KingDomain24@users.noreply.github.com>
2023-11-25 13:51:17 -0500

    Create Set all databases to auto-grow data files by 100MB.sql

---
+++ Configuration/Set all databases to auto-grow data files by 100MB.sql
@@ -0,0 +1,7 @@
+﻿-- Set all databases to auto-grow data files by 100MB
+-- Part of the SQL Server DBA Toolbox at https://github.com/DavidSchanzer/Sql-Server-DBA-Toolbox
+-- This script sets all user database data files to auto-grow by 100MB (instead of 10%)
+
+EXEC dbo.sp_foreachdb @command = 'DECLARE @sql VARCHAR(1000); SELECT @sql = ''ALTER DATABASE [?] MODIFY FILE ( NAME = N'''''' + name + '''''', FILEGROWTH = 102400KB )'' FROM [?].sys.master_files WHERE database_id = DB_ID(''?'') AND file_id = 1; EXEC(@sql);',
+                      @user_only = 1,
+                      @suppress_quotename = 1;
