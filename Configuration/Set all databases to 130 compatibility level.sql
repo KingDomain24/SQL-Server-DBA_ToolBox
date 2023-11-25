4b5072b37ca33317a05303ad975e1e9dd861e179
KingDomain24 <148955967+KingDomain24@users.noreply.github.com>
2023-11-25 13:48:15 -0500

    Create Set all databases to 130 compatibility level.sql

---
+++ Configuration/Set all databases to 130 compatibility level.sql
@@ -0,0 +1,6 @@
+﻿-- Set all databases to 130 compatibility level
+-- Part of the SQL Server DBA Toolbox at https://github.com/DavidSchanzer/Sql-Server-DBA-Toolbox
+-- This script sets all user databases to Compatibility Level 130 (SQL Server 2016)
+
+EXEC dbo.sp_foreachdb @command = 'ALTER DATABASE ? SET COMPATIBILITY_LEVEL = 130',
+                      @user_only = 1;
